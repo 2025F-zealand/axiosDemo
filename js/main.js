33fc034dee8cf3f10afd8c2fc0476d6617ced8e8
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2025-04-09 13:53:11 +0200

    tilføjet CDN til axios v-for loop button
    metode der bruger axios get

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
+const baseUri = "http://jsonplaceholder.typicode.com/posts"
+
 const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+            postslist:[],
         }
     },
     methods: {
-        myMethod(){
+        getAllPosts(){
+            axios.get(baseUri)
+            .then(response => {
+             console.log("in function getAllPosts");
+             console.log("status code: "+ response.status );
+
+             //add the returning data from the webservice to the variable "posts" that is an array
+              this.postslist = response.data;
+              
+             console.log("length of the posts array " + this.postslist.length)
+
+            })
+            .catch(error = (ex) => {
+               this.posts = []
+               this.error = ex.message
+              console.log("Error:" + this.error);
+            })      
+
+
+
+
+
 
         },
     },
